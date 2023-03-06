d663739c10d7c0b46d2e893bca1f63c2ed7e7e6f
Shota Iwamoto <amaginqoo@gmail.com>
2023-03-05 18:59:29 -0800

    Initial commit
    Generated by create-expo-app 1.3.1.

---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+  jsxSingleQuote: false,
+};
